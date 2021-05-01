87690747247f86d5352f08f6c6457664a91d9b24
brianlinggadjaja <brianlinggadjaja@outlook.com>
2021-05-01 04:59:36 -0700

    Add check for empty listingObj after scrape

--- scraper.js
+++ scraper.js
@@ -24,9 +24,9 @@ app.use(function(err, req, res, next) {
     res.end()
 });
 
-// app.listen(process.env.PORT, () => {
-//     console.log('Example app listening on port ' + process.env.PORT + '!')
-// })
+app.listen(process.env.PORT, () => {
+    console.log('Example app listening on port ' + process.env.PORT + '!')
+})
 
 async function scrapeProfessorInfo (profInfo) {
     // Atach base url to request
@@ -62,6 +62,9 @@ async function scrapeProfessorInfo (profInfo) {
                     }
                 })
 
+            // Check for empty listingsObj
+            listingsObj = (Object.keys(listingsObj).length !== 0) ? listingsObj : null
+            
             return listingsObj
         })
         .catch(function(error) {

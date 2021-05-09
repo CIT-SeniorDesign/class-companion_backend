7cf1c0a9d1588f4586e07448ddd9524a351d9337
brianlinggadjaja <brianlinggadjaja@outlook.com>
2021-05-09 02:21:53 -0700

    Initialize email verification microservice

--- microservices/notify-subscription/subscribe.js
+++ microservices/notify-subscription/subscribe.js
@@ -3,6 +3,7 @@ const env = require('dotenv').config()
 const cors = require('cors')
 const axios = require('axios')
 const express = require('express')
+const AWS = require('aws-sdk')
 const serverless = require('serverless-http')
 
 // Initalize express w/ dependencies
@@ -21,7 +22,58 @@ app.use(express.urlencoded({
     extended: true
 }))
 
-// Get professor info route
+
+// Create SES
+const ses = new AWS.SES({
+    region: 'us-east-1' 
+})
+
+
+app.post('/test', (req, res) => {
+    const params = {
+        Destination: {
+            ToAddresses: ['brianlinggadjaja98@gmail.com'],
+        },
+        Message: {
+        Body: {
+            Text: { Data: "Test" },
+        },
+        Subject: { Data: "Test Email" },
+        },
+        Source: "notifybrianlinggadjaja@gmail.com",
+    }
+
+    ses.sendEmail(params, (err, data) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(200)
+        }
+    })
+})
+
+app.post('/verifyemail', (req, res) => {
+console.log(req.body.email)
+    const params = {
+        EmailAddress: req.body.email, /* required */
+    }
+    
+    ses.verifyEmailAddress(params, function(err, data) {
+        if (err) {
+            res.send(err)
+        } else {
+            res.send(200)
+        }
+    })
+})
+
+
+/*
+    1. Verify email
+    2. Update
+    3. Subscribe
+*/
+// Subscribe
 app.post('/subscribe', async (req, res) => {
     console.log({
         'req': req,
@@ -46,3 +98,16 @@ if (process.env.NODE_ENV === 'instance') {
 
 // Attach serverless framework middleware to express app
 module.exports.handler = serverless(app)
+
+/*
+const dummyData = {
+    message: 'Hello World!'
+}
+
+fetch('https://ivp7n93c99.execute-api.us-east-1.amazonaws.com/test', {
+    method: 'POST',
+    body: JSON.stringify(dummyData)
+})
+.then(response => console.log(response))
+
+*/
